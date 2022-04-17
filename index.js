7488c5e69fa2ccc6b1b0cf1dc632e486592ea791
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-16 21:23:19 -0700

    commit tree ca4cc68a2f9634d91b66b4d08fced26688c2108b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name : "John",
+    streetAddress : "2900 Old Adobe Rd"
+}
+const employeeUsingSpreadOp = {...employee};
+
+function updateEmployeeWithKeyAndValue() {
+    let employeeUsingSpreadOp = {
+        name : "Sam",
+        streetAddress : "11 Broadway"
+    };
+    return employeeUsingSpreadOp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    return {
+        [employee.name] : "",
+    };
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+}
